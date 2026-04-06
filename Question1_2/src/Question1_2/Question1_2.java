a63716909a6c6610e1627708cff66278767dcd17
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-06 12:49:21 +0900

    演習 1_2＆1_3 直し

--- Question1_2/src/Question1_2/Question1_2.java
+++ Question1_2/src/Question1_2/Question1_2.java
@@ -4,15 +4,10 @@ public class Question1_2 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-	System.out.println("中");{
-		System.out.println("村");{
-			System.out.println("竜");{
-				System.out.println("生");{					
-					}
-				}
-			}
+	System.out.println("中\n村\n竜\n生");{
+		
 		}
-	}    	
+	}
 	}
 
 
--- Question1_3/src/Question1_3/Question1_3.java
+++ Question1_3/src/Question1_3/Question1_3.java
@@ -4,17 +4,11 @@ public class Question1_3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("中");{
-			System.out.println("村");{
-				System.out.println("");{
-					System.out.println("竜");{
-						System.out.println("生");{
+		System.out.println("中\n村");
+		System.out.println();
+		System.out.println("竜\n生");
 							
-						}
-					}
-				}
-			}
-		}    	
+		
 		}
 	}
 	
