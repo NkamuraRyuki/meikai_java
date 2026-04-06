14ea6477304578b62edda1324896e92d3ac3c87f
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-06 16:08:44 +0900

    演習4_2

--- ensyuu4_1/src/ensyuu4_1/enysuu4_1.java
+++ ensyuu4_1/src/ensyuu4_1/enysuu4_1.java
@@ -13,36 +13,49 @@ public class enysuu4_1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 Scanner standerdInput=new Scanner(System.in);
-//あらかじめ宣言
+//繰り返し入力するかどうか
 int retry;
+
 //繰り返し入力・表示できるようにする
 do{
+	
 //整数値を打ち込ませる
 System.out.println("整数値：");
+
 //変数に整数値を読み込ませる
 int judgementsigh=standerdInput.nextInt();
 
+
 //読み込んだ整数値によって画面に出力する内容を変えさせる
+
 //０より大きい場合
-if (judgementsigh>0) {
+if (judgementsigh>0) 
+{
 	//読み込んだ値が０より大きければ出力する
 	System.out.println("その値は正です。");
 }
+
 //０より小さい場合
 else if (judgementsigh<0){
+	
 	//読み込んだ値が０より小さければ出力する
 	System.out.println("その値は負です。");
 }
+
 //０の場合
 else {
+	
 	//読み込んだ値が０ならば出力する
 	System.out.println("その値は0です。");
 }
+
 //繰り返し入力するか促す
 System.out.print("もう一度行いますか？\n1...Yes/2...No:");
+
 //変数に読み込んだ値を代入
 retry=standerdInput.nextInt();
 }
+
 //もう一度入力。表示させる
 while (retry==1);
 	
---
+++ ensyuu4_2a/src/ensyuu4_2/ensyuu4_2.java
@@ -0,0 +1,53 @@
+package ensyuu4_2;
+
+/*
+ * クラス名:ensyu4_2
+ * 概要    :2桁の整数値での数当てゲーム
+ * 作成者  :中村竜生
+ * 作成日  :2026/04/06
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class ensyuu4_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+Random random=new Random();
+Scanner standardInput=new Scanner(System.in);
+
+
+//10~99の数字をランダムに生成
+int randomnumber=random.nextInt(90)+10;
+//ゲーム開始を知らせる
+System.out.println("数当てゲーム開始！");
+//ルールの説明
+System.out.println("10~99の数を当ててください。");
+//プレイヤーが入力した数字
+int guess;
+//正解するまで行う
+do {
+	//数字の入力を促す
+	System.out.println("いくつかな？");
+	//入力した数字を変数に代入
+	guess=standardInput.nextInt();
+	//乱数生成した数値と読み込んだ数値が等しいかどうか
+	//読み込んだ値が生成した値より小さい場合
+	if(guess<randomnumber) {
+		//より大きい値だと出力
+		System.out.println("もっと大きな数だよ。");
+	}
+	//読み込んだ値が生成した値より大きい場合
+	else if(guess>randomnumber) {
+		//より小さい値だと出力
+		System.out.println("もっと小さな数だよ。");}
+	}
+//生成した値と入力した値が一致するまでループ
+while(guess!=randomnumber);
+
+//2つの値が一致したことを出力
+System.out.println("正解です。");
+
+	}
+
+}
