0908512fe54f56b69b47a6e9b740a762c54d76e3
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-07 15:36:25 +0900

    演習4_2

--- ensyuu4_2/src/ensyuu4_2/ensyuu4_2.java
+++ ensyuu4_2/src/ensyuu4_2/ensyuu4_2.java
@@ -14,38 +14,39 @@ public class ensyuu4_2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 //乱数を生成する
-Random standardrandom=new Random();
+Random standardRandom=new Random();
 //キーボードからの入力を読み込む
 Scanner standardInput=new Scanner(System.in);
 
 
 //10~99の数字をランダムに生成
-int randomnumber=standardrandom.nextInt(90)+10;
+int randomNumber=standardRandom.nextInt(90)+10;
 //ゲーム開始を知らせる
 System.out.println("数当てゲーム開始！");
 //ルールの説明
 System.out.println("10~99の数を当ててください。");
 //プレイヤーが入力した数字
-int guess;
+int guessNumber;
+
 //正解するまで行う
 do {
 	//数字の入力を促す
 	System.out.println("いくつかな？");
 	//入力した数字を変数に代入
-	guess=standardInput.nextInt();
+	guessNumber=standardInput.nextInt();
+	
 	//乱数生成した数値と読み込んだ数値が等しいかどうか
 	//読み込んだ値が生成した値より小さい場合
-	if(guess<randomnumber) {
+	if(guessNumber<randomNumber) {
 		//より大きい値だと出力
 		System.out.println("もっと大きな数だよ。");
-	}
-	//読み込んだ値が生成した値より大きい場合
-	else if(guess>randomnumber) {
+	}//読み込んだ値が生成した値より大きい場合//
+	else if(guessNumber>randomNumber) {
 		//より小さい値だと出力
-		System.out.println("もっと小さな数だよ。");}
+		System.out.println("もっと小さな数だよ。");
 	}
-//生成した値と入力した値が一致するまでループ
-while(guess!=randomnumber);
+}//生成した値と入力した値が一致するまでループ
+while(guessNumber!=randomNumber);
 
 //2つの値が一致したことを出力
 System.out.println("正解です。");
