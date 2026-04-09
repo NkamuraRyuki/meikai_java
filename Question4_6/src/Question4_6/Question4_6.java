3127fdca44cbc90fa1dd69c4c7f136a80220574a
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-09 11:05:09 +0900

    演習4_6

--- Question4_6/src/Question4_6/Question4_6.java
+++ Question4_6/src/Question4_6/Question4_6.java
@@ -13,70 +13,69 @@ import java.util.Scanner;
 public class Question4_6 {
 
 	/*
-     * 関数名           :main
-     * メソッドの説明   :main処理をするための関数
-     * パラメータの説明 :なし
-     * 返り値           :なし
-     * 作成者  			:中村竜生
-     * 作成日  			:2026.04.08.
-     */
-	
+	 * 関数名           :main
+	 * メソッドの説明   :main処理をするための関数
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者  			:中村竜生
+	 * 作成日  			:2026.04.08.
+	 */
+
 	public static void main(String[] args) {
-		
+
 		/*List4_7の書き換え*/
 		//キーボードからの入力を読み込む
 		Scanner standardInput = new Scanner(System.in);
-		//このプリグラムが何をするのか提示する
+		//このプログラムが何をするのか提示する
 		System.out.println("何個*を表示しますか：");
 		//入力された数字を読み込む
 		int inputNumber = standardInput.nextInt();
-		
+
 		//while文で使う変数を初期化
 		int countUp = 0;
-		
+
 		//入力された数だけ*を表示する
 		while(countUp < inputNumber) {
 			//'*'を出力
 			System.out.println('*');
-			//入力した数字に1足す
+			//countUpに1足す
 			countUp ++;
 		}
 		//1未満であれば改行文字を出力しないようにして、入力された数だけ*を表示する
 		while(countUp > inputNumber) {
 			//'*'を出力
 			System.out.print('*');
-			//入力した数字に1足す
+			//countUpに1足す
 			countUp --;
 		}
-		
-		
+
+
 		/*List4_8の書き換え*/
 		//キーボードからの入力を読み込む
 		Scanner standardInput = new Scanner(System.in);
-		//このプリグラムが何をするのか提示する
+		//このプログラムが何をするのか提示する
 		System.out.println("何個*を表示しますか：");
 		//入力された数字を読み込む
 		int inputNumber = standardInput.nextInt();
-		
+
 		//while文で使う変数を初期化
 		int countUp = 1;
-		
+
 		//入力された数だけ*を表示する
 		while(countUp <= inputNumber) {
 			//'*'を出力
 			System.out.println('*');
-			//入力した数字に1足す
+			//countUpに1足す
 			countUp ++;
 		}
 		//1未満であれば改行文字を出力しないようにして、入力された数だけ*を表示する
 		while(countUp-1 > inputNumber) {
 			//'*'を出力
 			System.out.print('*');
-			//入力した数字に1引く
+			//countUpに1引く
 			countUp --;
 		}
-		
-		
+
 	}
 
 }
