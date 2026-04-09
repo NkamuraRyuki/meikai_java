c553a7dd97512088e38091a5dca25b212840b8e6
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-09 11:04:10 +0900

    演習4_3

--- ensyuu4_3/src/ensyuu4_3/ensyuu4_3.java
+++ ensyuu4_3/src/ensyuu4_3/ensyuu4_3.java
@@ -8,43 +8,46 @@ package ensyuu4_3;
  */
 import java.util.Scanner;
 class ensyuu4_3 {
-	
+
 	/*
-     * 関数名           :main
-     * メソッドの説明   :main処理をするための関数
-     * パラメータの説明 :なし
-     * 返り値           :なし
-     * 作成者  			:中村竜生
-     * 作成日  			:2026.04.08.
-     */
+	 * 関数名           :main
+	 * メソッドの説明   :main処理をするための関数
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者  			:中村竜生
+	 * 作成日  			:2026.04.08.
+	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//キーボードからの入力を読み込む
+		Scanner standardInput = new Scanner(System.in);
 
-Scanner standardInput=new Scanner(System.in);
+		//整数Aの入力を促す
+		System.out.println("整数A：");
 
-//整数Aの入力を促す
-System.out.println("整数A：");
-//読み込んだ整数で初期化
-int firstnumber=standardInput.nextInt();
+		//読み込んだ整数で初期化
+		int firstnumber = standardInput.nextInt();
 
-//整数Bの入力を促す
-System.out.println("整数B：");
-//読み込んだ整数で初期化
-int secondnumber=standardInput.nextInt();
+		//整数Bの入力を促す
+		System.out.println("整数B：");
 
-//小さい方の数字を判定
-int startmin=(firstnumber<secondnumber)?firstnumber:secondnumber;
-//大きい方の数字を判定
-int endmax=(firstnumber>secondnumber)?firstnumber:secondnumber;
-//小さい方の数字が大きい方の数字と等しくなるまで＋1を繰り返す
-do {
-	//最小値を出力する
-	System.out.println(startmin);
-		//最小値に＋1する
-        startmin=startmin+1;
-    } 
-//大きいほうの数字と等しくなるまで続ける
-while(startmin<=endmax);
+		//読み込んだ整数で初期化
+		int secondnumber = standardInput.nextInt();
 
-}
+		//小さい方の数字を判定
+		int startmin = (firstnumber < secondnumber) ? firstnumber : secondnumber;
+
+		//大きい方の数字を判定
+		int endmax = (firstnumber > secondnumber) ? firstnumber : secondnumber;
+
+		//小さい方の数字が大きい方の数字と等しくなるまで＋1を繰り返す
+		do {
+			//最小値を出力する
+			System.out.println(startmin);
+
+			//最小値に＋1する
+			startmin=startmin+1;
+
+			//大きいほうの数字と等しくなるまで続ける
+		}while(startmin<=endmax);
+	}
 }
