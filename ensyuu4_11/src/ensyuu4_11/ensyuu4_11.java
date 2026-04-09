3c739bfef4cbbd2f165bf499aa2350b56e991f26
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-09 15:14:16 +0900

    演習4_11完了

---
+++ ensyuu4_11/src/ensyuu4_11/ensyuu4_11.java
@@ -0,0 +1,48 @@
+package ensyuu4_11;
+
+//キーボードから入力した値を読み込むようにする
+import java.util.Scanner;
+
+/*
+ * クラス名:ensyuu4_11
+ * 概要    :正の整数値を0までカウントダウンするList4_4をfou文で作成
+ * 作成者  :中村竜生
+ * 作成日  :2026/04/09
+ */
+public class ensyuu4_11 {
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :main処理をするための関数
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者  			:中村竜生
+	 * 作成日  			:2026.04.09.
+	 */
+	public static void main(String[] args) {
+
+		//キーボードからの入力を読み込む
+		Scanner standardInput = new Scanner(System.in);
+
+		//実施される内容を表示する
+		System.out.println("カウントダウンします。");
+
+		//入力された数字を格納する
+		int inputNumber = standardInput.nextInt();
+
+		//正の整数値以外を対策
+		for(; inputNumber <= 0 ;inputNumber = standardInput.nextInt()) {
+			//正の整数値を入力させる
+			System.out.println("正の整数値を入力してください。");
+		}
+
+		//入力された数字から1ずつ減らす
+		for (int countDown = inputNumber; countDown >= 0 ; countDown --) {
+
+			//出力する
+			System.out.println(countDown);
+		}
+
+
+	}
+
+}
