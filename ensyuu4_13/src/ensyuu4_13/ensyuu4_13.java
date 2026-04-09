40d57718b77cd24a50a6bfdfc802ad7aa77db178
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-09 15:32:10 +0900

    演習4_13完了

---
+++ ensyuu4_13/src/ensyuu4_13/ensyuu4_13.java
@@ -0,0 +1,53 @@
+package ensyuu4_13;
+
+//キーボードから入力した値を読み込むようにする
+import java.util.Scanner;
+
+/*
+ * クラス名:ensyuu4_13
+ * 概要    :1からnまでの和を求めるList4_10をfor文で作成
+ * 作成者  :中村竜生
+ * 作成日  :2026/04/09
+ */
+public class ensyuu4_13 {
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :main処理をするための関数
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者  			:中村竜生
+	 * 作成日  			:2026.04.09.
+	 */
+	public static void main(String[] args) {
+
+		//キーボードからの入力を読み込む
+		Scanner standardInput = new Scanner(System.in);
+
+		//実施される内容を表示する
+		System.out.println("1から入力した数までの和を求めます。");
+
+		//入力された数字を格納する
+		int inputNumber = standardInput.nextInt();
+		
+		//和を格納する
+		int sumNumber = 0; 
+
+		//正の整数値以外を対策
+		for(; inputNumber <= 0 ;inputNumber = standardInput.nextInt()){
+			//正の整数値を入力させる
+			System.out.println("正の整数値を入力してください。");
+		}
+		
+		//1から入力された数字までの整数を全て足す
+		for (int countUp = 0;countUp <= inputNumber;countUp ++) {
+			
+			//和を求める
+			sumNumber += countUp;
+		}
+		
+		//和を出力する
+		System.out.println(sumNumber);
+		
+	}
+
+}
