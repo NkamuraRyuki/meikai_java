b0d5c1dc3c8aba64239949d4b728f5b30d875b9b
NkamuraRyuki <ddt.rk.nakamura@mail.com>
2026-04-09 11:04:40 +0900

    演習4_5

--- ensyuu4_5/src/ensyuu4_5/ensyuu4_5.java
+++ ensyuu4_5/src/ensyuu4_5/ensyuu4_5.java
@@ -12,35 +12,38 @@ import java.util.Scanner;
 public class ensyuu4_5 {
 
 	/*
-     * 関数名           :main
-     * メソッドの説明   :main処理をするための関数
-     * パラメータの説明 :なし
-     * 返り値           :なし
-     * 作成者  			:中村竜生
-     * 作成日  			:2026.04.08.
-     */
-	
+	 * 関数名           :main
+	 * メソッドの説明   :main処理をするための関数
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者  			:中村竜生
+	 * 作成日  			:2026.04.08.
+	 */
+
 	public static void main(String[] args) {
 		//キーボードからの入力を読み込む
 		Scanner standardInput=new Scanner(System.in);
-		//このプリグラムが何をするのか提示する
+
+		//このプログラムが何をするのか提示する
 		System.out.println("カウントダウンします。");
+
 		//入力された数字
 		int InputNumber =0;
-		//正の整数値が入力されるまで繰り返す
+
+		//正の整数値を入力させる
 		do {
 			//正の整数値の入力を促す
 			System.out.println("正の整数値：");
 			//入力された数字を代入
 			InputNumber = standardInput.nextInt();
-			
-		//正の整数値が入力されたらdo文から脱け出す
+
+			//正の整数値が入力されるまで繰り返す
 		}while (InputNumber <= 0);
-		
+
 		while (InputNumber >= 0){
 			System.out.println(--InputNumber);
 		}
-		
+
 	}
 
 }
